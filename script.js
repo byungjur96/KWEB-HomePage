6960bce2851864d3d32d6e57328ce6b0f8f16428
Byungju Chae <cbyungjub@naver.com>
2019-07-28 03:35:12 +0900

    방명록 디자인 개선 방명록 empty string 예외 처리 JavaScript 코드 주석 추가

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function memoUpload() {
     if (text.length > 400) {
         window.alert("방명록은 400자를 초과할 수 없습니다.");
     }
+    else if (text.length === 0) {
+        window.alert("방명록을 작성해 주세요.");
+    }
     else {
         let currentTime = new Date();
         let time = `${currentTime.getFullYear()}-${currentTime.getMonth()}-${currentTime.getDate()}`;
@@ -24,6 +27,7 @@ function countLength() {
     viewLen.innerHTML = text.length;
 }
 
+// 화면이 랜딩페이지를 벗어나면 navigation bar를 수정한다.
 function stickyNav() {
     let nav = document.getElementById("nav-bar");
     let landingHeight = document.getElementById("title").offsetHeight - 64;
@@ -35,15 +39,16 @@ function stickyNav() {
     }
 }
 
-// 화면 로딩 시 navigation bar
+// 화면 로딩 시 navigation bar의 디자인을 확인한다.
 stickyNav();
 
-// 필요한 element들 선언
+// 필요한 element들 선언한다.
 let guestBook = document.getElementById("guest-book");
 let uploadBtn = document.getElementById("upload-btn");
 let categories = document.getElementsByClassName("nav-category");
 
-// 호출한 element들에 event listner 적용
+
+// 호출한 element들에 event listner를 적용한다.
 guestBook.addEventListener('keyup', () => countLength());
 uploadBtn.addEventListener('click', () => memoUpload());
 document.addEventListener('scroll', () => stickyNav());
